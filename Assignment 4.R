7cd707f720d1cdba3c532124f9edd3ff28df3fba
Annayathamma <98015403+Annayathamma@users.noreply.github.com>
2022-03-08 10:03:12 +0100

    Update Assignment 4.R
    add library

--- Assignment 4.R
+++ Assignment 4.R
@@ -1,1 +1,15 @@
 #line 1
+library(ggplot2)
+library(ggfortify)
+library(forecast)
+library(fpp3)
+library(tidyr)
+library(fable)
+library(tsibble)
+library(AER)
+library(MASS)
+library(caret)
+
+vic_elec
+head(vic_elec)
+vic_elec %>% 
