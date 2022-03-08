cb0817ba902d6ac35c32dac566d88da7f2960e7e
Annayathamma <98015403+Annayathamma@users.noreply.github.com>
2022-03-08 10:34:11 +0100

    Update Assignment 4.R

--- Assignment 4.R
+++ Assignment 4.R
@@ -16,4 +16,20 @@ head(vic_elec)
 jan_vic_elec<- vic_elec %>%
   filter(yearmonth(Time)== yearmonth("2014 Jan")) %>%
   index_by(Date= as_date(Time)) %>%
-  summarise(Demand= sum(Demand), Temperature
+  summarise(Demand= sum(Demand), Temperature = max(Temperature))
+
+# a. Plot the data and find the regression model for Demand with temperature as an explanatory variable. Why
+# is there a positive relationship?
+jan_vic_elec %>% autoplot()
+#fit a regression model
+model <- lm(Demand~Temperature, data=jan_vic_elec)
+
+#b.Produce a residual plot. Is the model adequate? Are there any outliers or influential observations?
+
+#get list of residuals 
+res <- resid(model)
+plot(fitted(model), res)
+qqnorm(res)
+qqline(res)
+plot(density(res))
+#ddaacda
