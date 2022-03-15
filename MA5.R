2826aefc3282ca11fe75cfbfa018d952937a4eb4
rainbowjoy <dadu21ab@student.cbs.dk>
2022-03-15 10:14:16 +0100

    why

--- MA5.R
+++ MA5.R
@@ -1,2 +1,9 @@
-DId this work
+###*Question 1*
+###Code needed from a & b:
 
+jan_vic_elec <- vic_elec %>%
+  filter(yearmonth(Time) == yearmonth("2014 Jan")) %>%
+  index_by(Date = as_date(Time)) %>%
+  summarise(Demand = sum(Demand), Temperature = max(Temperature))
+fit <- jan_vic_elec %>%
+  model(TSLM(Demand ~ Temperature))
