953200b2cf6e775f1e82b351abaf35975fa4670f
rainbowjoy <dadu21ab@student.cbs.dk>
2022-03-15 10:22:40 +0100

    Question 1

--- MA5.R
+++ MA5.R
@@ -1,10 +1,18 @@
 ###*Question 1*
 ###Code needed from a & b:
 
+library(fpp3)
+
 jan_vic_elec <- vic_elec %>%
   filter(yearmonth(Time) == yearmonth("2014 Jan")) %>%
   index_by(Date = as_date(Time)) %>%
   summarise(Demand = sum(Demand), Temperature = max(Temperature))
+
+jan_vic_elec
+
 fit <- jan_vic_elec %>%
   model(TSLM(Demand ~ Temperature))
 
+#forecast 15
+
+fit %>% forecast(h=2) %>% autoplot()
